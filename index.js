55886a577dfdc7273455c755c53d20b3c3a5ba06
anvar-muzaffarli <prof.anvar2013@gmail.com>
2022-09-25 07:58:48 +0400

    faktoriyalin hesablanmasi meselesini bitirdik

---
+++ index.js
@@ -0,0 +1,414 @@
+// // // // console.log("Hello students")
+
+// // // // let myAge = 25
+
+// // // // console.log(myAge)
+// // // // console.error("Sehv panele girish etdiniz")
+// // // // console.warn("Sehv panele girish etdiniz")
+// // // // console.clear()
+
+// // // // console.table({ad: "Anvar", soyad: "Mizanfarli", age:25})
+
+// // // // console.time("id")
+
+// // // // console.log("sdfdsfds")
+// // // // console.log("sdfdsfdsgdfgfdgfdds")
+// // // // console.log("sdfdsdsfdsfdsfds")
+// // // // console.timeEnd("id")
+// // // // console.clear()
+
+// // // // let eded = 25
+
+// // // // alert(eded)
+
+// // // // let daxilEdilmishDeyer = prompt("Bir eded daxil edin", 25)
+
+// // // // console.log(daxilEdilmishDeyer)
+
+// // // // single line
+
+// // // /*
+// // // m
+// // // l
+// // // i
+
+// // // */
+
+// // // // statement ve deyishen anlayishinin izahi
+// // // //  var let const
+// // // // var yasim = 25
+
+// // // // var yasim = 37
+// // // // redeclare - yeniden teyin etmek
+
+// // // // var yasim = 35
+
+// // // // yasim = 44
+
+// // // // console.log(yasim)
+
+
+// // // // let ve const
+// // // // const telebeninAdi = "Anvar"
+
+// // // //   telebeninAdi = "Serdar"
+
+// // // //  console.log(telebeninAdi);
+
+
+// // // // console.log(yasim+50)
+
+
+// // // // const piReqemi = 3.14
+// // // // console.log(piReqemi)
+
+// // // // primitive Number String Boolean undefined null symbol
+// // // // referans tipler object
+
+// // // // number 
+
+// // // // let number = 25
+
+// // // // console.log(number)
+// // // // // console.log(typeof number)
+
+
+// // // // let onluqEded = 2.5
+// // // // console.log(onluqEded);
+
+
+// // // // let name = `Babek`
+// // // // let lastName = `Kerimli`
+
+// // // // console.log(typeof name)
+
+// // // // let fullName = name + ' ' + lastName
+
+// // // // console.log(fullName)
+
+// // // // const rey = name  + " " + lastName+ " ikinci defe \"MCT statusuna\" layiq goruldu"
+
+// // // // const rey2 = ` ${name} ${lastName}   Ikinci defe MCT statusuna layiq goruldu`
+
+// // // // console.log(rey2);
+
+
+// // // // let cutEdeddirMi = false
+// // // // console.log(typeof cutEdeddirMi)
+
+
+// // // // let gpa
+
+// // // // gpa = 98.54
+
+// // // // console.log(gpa)
+
+// // // // null 
+
+// // // // console.log(Number(null))
+// // // // // null object olmasi js de bug hesab olunur
+// // // // console.log(typeof null)
+// // // // NaN Not a Number
+// // // // console.log(typeof Number("Mehdi Abasov"))
+
+
+// // // // truthy falsey
+
+
+// // // // let tevellud = 1997
+
+// // // // tevellud = "Anvar"
+
+// // // // if(tevellud*99) {
+// // // //     console.log("true")
+// // // // }
+
+// // // // else {
+// // // //     console.log("false")
+// // // // }
+
+
+// // // // false,0, "", null,undefined, NaN falsey
+
+
+// // // // let eded1 = 60
+// // // // let eded2 = "37"
+
+// // // // let netice = eded1 %eded2
+
+// // // // console.log(netice);
+
+// // // // x = 5
+
+
+// // // // x = x+25
+
+// // // // x += 25
+
+// // // // let x = 1
+// // // // console.log(x)
+
+// // // // // x++ //increment
+// // // // ++x
+
+// // // // console.log(++x)
+// // // // console.log(++x)
+// // // // console.log(x)
+
+
+// // // // // decrement --
+// // // // console.log(x--) //3
+// // // // console.log(x) //2
+
+
+// // // let number1 = 35
+// // // // let number2 = 35
+
+// // // // console.log(number1 !== number2)
+// // // // && 11 1 
+// // // console.log((number1 <25 || number2 >= 35))
+
+
+// // // let menimGPAm = 81 , onunGPAsi = 97
+// // // // true false
+// // // if(menimGPAm > onunGPAsi) {
+// // //     console.log("Sen qebul oldun")
+// // // }
+
+// // // else if(menimGPAm === onunGPAsi) {
+// // //     console.log("Qebul baliniz yoxlamadadir")
+// // // }
+
+// // // else {
+// // //     console.log("Teessuf ki qebul olmadiniz")
+// // // }
+
+// // // if(25==="25") {
+// // //     console.log("dogrudur")
+// // // } else {
+// // //     console.log("Yanlishdir")
+// // // }
+
+
+// // // let cutEdeddirMi = true 
+
+
+// // // if(cutEdeddirMi===false) {
+// // //     console.log("yanlish")
+// // // }
+// // // else {
+// // //     console.log("Dogru")
+// // // }
+// // // ternary operator
+
+// // // cutEdeddirMi ? console.log("dogru") : console.log("Yanlish")
+
+// // // console.log("Salam")
+// // // console.log("Salam")
+// // // console.log("Salam")
+// // // console.log("Salam")
+// // // console.log("Salam")
+// // // console.log("Salam")
+// // // dovr operatorlari
+// // // for 
+
+// // // for(ilk menimsetdiyimizdeyer; calisma sherti; calishmadansonraneolacaq) {
+
+// // // }
+
+
+// // // for(;;) // sonsuz dovr
+
+// // // let j =0
+
+
+// // // for(let i =0, j=0; (i+j)<150; i=i+5, j=j+10) {
+// // //     console.log(`i-nin deyeri :${i} ve j-nin deyeri ${j}`)
+// // // }   
+
+// // // let number = 25
+
+
+// // // while(number < 45) {
+// // //     console.log(`${number}-ci Salam dunya`)
+// // //     number++
+// // // }
+
+// // // let yas = 45
+
+
+// // // do {
+// // //    console.log(`${yas} lorem`) 
+// // //    yas++
+// // // }
+
+// // // while(yas <45)
+
+// // // break continue return
+
+// // // for(let i=0; i<45; i++) {
+// // //     if(i==12) {
+// //         // return;
+// //         // illegal return statement
+// // //     }
+
+// // //     console.log(`${i}-ci index Break istifadesi`)
+// // // }
+
+// // // switch case
+
+// // let hefteninGunu = 5
+
+
+// // switch(hefteninGunu) {
+// //     case 1: console.log("Bazar ertesi"); break;
+// //     case 2: console.log("Cersenbe axshami"); break;
+// //     case 3: console.log("Cershembe"); break;
+// //     case 4: console.log("Cume axshami"); break;
+// //     case 5: console.log("Cume"); break;
+// //     case 6: console.log("Senbe"); break;
+// //     case 7: console.log("Bazar"); break;
+// //     default: console.log("Yanlish secim")
+// // }
+
+// // console.log("Switch bitdi")
+
+// // let saniyeString = prompt("Saniye deyerini daxil et", 100)
+
+// // console.log(typeof saniyeString)
+
+// // let saniye = parseInt(saniyeString)
+
+// // let deqiqe = parseInt(saniye/60)
+
+// // console.log(deqiqe)
+
+// // let qalanSaniye = saniye % 60
+
+// // console.log(`Girilen saniye deyerine (${saniye}) uygun olaraq netice : ${deqiqe} deqiqe ${qalanSaniye} saniyedir`)
+
+// // ikinci task
+// // let daxilEdilenDerece = parseInt(prompt("Fahrenheit daxil edin", 100))
+
+// // let celciusDerece = (5/9)*(daxilEdilenDerece-32)
+
+// // console.log(`Girdiyin derece : ${daxilEdilenDerece} Netice beledir : ${celciusDerece.toFixed(2)}`)
+// // ucuncu task
+
+
+// // let daxilEdilmishIl = parseInt(prompt("Il deyerini daxil et", 2022))
+
+
+// // let artiqIldirMi = ((daxilEdilmishIl%400===0)|| (daxilEdilmishIl%4===0 && daxilEdilmishIl%100 !=0 ))
+
+
+// // console.log(`Daxil edilmish il ${daxilEdilmishIl} artiq ildir mi? ${artiqIldirMi}`)
+
+// // birinci tapsiriq 
+
+// // let birinciEded = parseInt(prompt("Birinci ededi daxil edin", 1))
+// // let ikinciEded = parseInt(prompt("Ikinci ededi daxil edin", 1))
+// // let ucuncuEded = parseInt(prompt("Ucuncu ededi daxil edin", 1))
+
+
+// // let ortalama = (birinciEded + ikinciEded+ucuncuEded)/3
+
+// // alert("Daxil etdiyiniz ededlerin ededi ortasi : " + ortalama)
+
+// // let birinciTeref = parseInt(prompt("Birinci terefin olcusunu daxil et", 20))
+// // let ikinciTeref = parseInt(prompt("Ikinci terefin olcusunu daxil et", 20))
+// // let ucuncuTeref = parseInt(prompt("Ucuncu terefin olcusunu daxil et", 20))
+
+// // if((birinciTeref == ikinciTeref) && (birinciTeref == ucuncuTeref)) {
+// //     alert("Beraber terefli ucbucaq")
+// // }
+// // else if(birinciTeref !=ikinciTeref && birinciTeref !=ucuncuTeref && ikinciTeref !=ucuncuTeref) {
+// //         alert("Muxtelif terefli ucbucaq")
+// // }
+
+// // else {
+// //     alert("Beraberyanli ucbucaq")
+// // }
+
+
+// // let imtahanaQederkiQiymet = parseInt(prompt("Imtahana qederki bali daxil et", 70))
+// // let imtahanSonrakiBal = parseInt(prompt("Imtahandan qazanilan bal daxil et", 30))
+
+// // let ortalama = imtahanaQederkiQiymet*0.5 + imtahanSonrakiBal*0.5
+
+// // if(ortalama >= 51) {
+// //     alert("Tebrikler! Kecdin")
+// // }
+
+// // else {
+// //     alert("Kesildin!!!")
+// // }
+
+// for(let i=0; i< 6; i++) {
+//     console.log(`${i}-ci indexdeki ad : Anvar`)
+// }
+
+// let i =0
+
+// while(i <6) {
+//     console.log("ANvar")
+//     i++
+// }
+// // block scope elementler yeniden teyini oluna bilmez
+// let i2 = 0
+
+// do {
+//     console.log(`${i2+1}-ci indexdeki ad : Anvar`)
+//     i2++
+// }
+
+// while (i2 < 6)
+
+
+// let cemi = 0
+
+// for(let i=0; i< 100; i++) {
+//     cemi = cemi + i
+
+//     console.log(cemi)
+// }
+
+// console.log(`1-den 100e qeder ededlerin cemi ${cemi}`)
+
+
+// let yazdirilacaqMetn = ""
+
+// for(let i=1; i<=10; i++) {
+//     // if(i != 10) {
+//     //     yazdirilacaqMetn = yazdirilacaqMetn + i + ","
+//     // }
+
+//     // else {
+//     //     yazdirilacaqMetn = yazdirilacaqMetn + i
+//     // }
+
+//     if(i!=10) {
+//         yazdirilacaqMetn = yazdirilacaqMetn + i + ","
+//     }
+//     else {
+//         yazdirilacaqMetn = yazdirilacaqMetn + i
+//     }
+
+// }
+
+// console.log(yazdirilacaqMetn)
+
+
+
+
+
+
+// let number = parseInt(prompt("Faktoriyali hesablanacaq ededi daxil et", 10))
+
+// let faktoriyal = 1
+
+// for(let i=1; i<=number; i++) {
+//     faktoriyal = faktoriyal*i
+// }
+
+// console.log(`Girilen ededin faktoriyali ${faktoriyal}`)
